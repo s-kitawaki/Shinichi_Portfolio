89ae723341692a9a8ba31898a4040bafa87dcb8a
s-kitawaki <b1a2b1o4@gmail.com>
2024-07-13 21:34:35 +0900

    コンポーネント間の値の受け渡し実装

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <v-app>
     <v-main>
+        <div>
+          <p>testでーす</p>
+        </div>
       <HelloWorld/>
+      // 違うコンポーネントに値を引き渡す表記方法
+      <TestComponents :number="number"/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TestComponents from './components/TestComponents.vue'
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    TestComponents,
   },
-
+  // data()はどちらでも同じ
   data: () => ({
-    //
+    number: 100,
   }),
+  // data() {
+  //   return {
+  //     number: 1,
+  //   };
+  // }
 }
 </script>
---
+++ src/components/TestComponents.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <div>
+      <h2>Props.vue</h2>
+      <p>子コンポーネントに渡された値:({{ number }})</p>
+    </div>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'TestComponents',
+  props: ['number'],
+
+  data: () => ({
+
+  }),
+}
+</script>
